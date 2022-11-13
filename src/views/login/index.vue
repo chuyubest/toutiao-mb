dd1f7643351432e68704dcad68d4b9f7d247e279
chuyu <1575057401@qq.com>
2022-11-13 19:53:15 +0800

    修改登陆页面的样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,62 @@
 <template>
-    <div class="login-container">
-        登陆页面
-    </div>
+  <div class="login-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar" title="登录" />
+    <!-- form表单 -->
+    <van-form @submit="onSubmit">
+      <van-field name="用户名" placeholder="请输入手机号">
+        <i class="iconfont icon-shouji" slot="left-icon"></i>
+      </van-field>
+      <van-field type="password" name="验证码" placeholder="请输入验证码">
+        <i class="iconfont icon-yanzhengma" slot="left-icon"></i>
+        <template #button>
+          <van-button class="send-sms-btn" round  size="small" type="default">获取验证码</van-button>
+        </template>
+      </van-field>
+      <div class="login-btn-wrap">
+        <van-button class="login-btn" round block type="info" native-type="submit">登录</van-button
+        >
+      </div>
+    </van-form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    onSubmit (values) {
+      console.log('submit', values)
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less">
+.login-container {
+  .iconfont {
+    font-size: 37px;
+  }
+  .send-sms-btn{
+    width: 160px;
+    height: 50px;
+    line-height: 50px;
+    font-size: 10px;
+    color: #666;
+    background-color: #ededed;
+  }
+  .login-btn-wrap{
+    padding: 53px 33px;
+    .login-btn{
+        background-color: #6db4fb;
+        border: none;
+    }
+  }
+}
 </style>

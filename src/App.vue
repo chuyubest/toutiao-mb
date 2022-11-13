22e612ccd0254f07c7988185e130763be50c25c0
chuyu <1575057401@qq.com>
2022-11-13 18:38:40 +0800

    完成登陆页面组件创建以及配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div id="app">
     <router-view/>
-    黑马头条
-    <i class="iconfont icon-yanzhengma"></i>
-    <van-button type="info">信息按钮</van-button>
-    <div class="box"></div>
-
   </div>
 </template>
 <script>
@@ -14,9 +9,5 @@ export default {
 }
 </script>
 <style lang="less">
-  .box {
-    width: 200px;
-    height: 200px;
-    background-color: pink;
-  }
+
 </style>

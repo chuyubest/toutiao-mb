1c6fe79fdb8dba26097c3961f7a0ba725dc6a658
chuyu <1575057401@qq.com>
2022-11-14 17:43:42 +0800

    完成验证码倒计时功能

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,28 +4,47 @@
     <van-nav-bar class="page-nav-bar" title="登录" />
     <!-- form表单 -->
     <!-- 表单验证:给vanField配置rules验证规则 当表单提交的时候会自动触发 如果通过会触发submit事件 失败不会提交 -->
-    <van-form @submit="onSubmit">
-      <van-field name="用户名" placeholder="请输入手机号" v-model="user.mobile" type="number" maxlength="11"
-      :rules="rules.mobile">
+    <van-form @submit="onSubmit" ref="loginForm">
+      <van-field
+        name="mobile"
+        placeholder="请输入手机号"
+        v-model="user.mobile"
+        type="number"
+        maxlength="11"
+        :rules="rules.mobile"
+      >
         <i class="iconfont icon-shouji" slot="left-icon"></i>
       </van-field>
-      <van-field name="验证码" placeholder="请输入验证码" v-model="user.code" :rules="rules.code" type="number" maxlength="6">
+      <van-field
+        name="code"
+        placeholder="请输入验证码"
+        v-model="user.code"
+        :rules="rules.code"
+        type="number"
+        maxlength="6"
+      >
         <i class="iconfont icon-yanzhengma" slot="left-icon"></i>
         <template #button>
-          <van-button class="send-sms-btn" round size="small" type="default"
+          <van-count-down
+            :time="1000 * 10"
+            format="ss s"
+            v-if="isCountDownShow"
+            @finish="isCountDownShow = false"
+          />
+          <van-button
+            v-else
+            class="send-sms-btn"
+            round
+            size="small"
+            type="default"
+            native-type="button"
+            @click="onSendSms"
             >获取验证码</van-button
           >
         </template>
       </van-field>
       <div class="login-btn-wrap">
-        <van-button
-          class="login-btn"
-          round
-          block
-          type="info"
-          native-type="submit"
-          >登录</van-button
-        >
+        <van-button class="login-btn" round block type="info">登录</van-button>
       </div>
     </van-form>
   </div>
@@ -42,10 +61,16 @@ export default {
         code: '' // 验证码
       },
       rules: {
-        mobile: [{ required: true, message: '手机号不能为空', trigger: 'OnBlur' },
-          { pattern: /1[3|5|7|8]\d{9}/, message: '手机号格式错误' }],
-        code: [{ required: true, message: '请填写验证码', trigger: 'OnBlur' }, { pattern: /^\d{6}$/, message: '验证码格式错误' }]
-      }
+        mobile: [
+          { required: true, message: '手机号不能为空', trigger: 'OnBlur' },
+          { pattern: /1[3|5|7|8]\d{9}/, message: '手机号格式错误' }
+        ],
+        code: [
+          { required: true, message: '请填写验证码' },
+          { pattern: /^\d{6}$/, message: '验证码格式错误' }
+        ]
+      },
+      isCountDownShow: false // 是否显示倒计时
     }
   },
   methods: {
@@ -70,8 +95,19 @@ export default {
           this.$toast.fail('登录失败,请稍后重试')
         }
       }
-
       // 4.根据响应结果处理后续请求
+    },
+    // 发送验证码
+    async onSendSms () {
+      // 1.校验手机号码
+      try {
+        await this.$refs.loginForm.validate('mobile')
+        // 2.验证通过显示倒计时
+        this.isCountDownShow = true
+        // 3.请求发送验证码
+      } catch (error) {
+        console.log('验证失败', error)
+      }
     }
   }
 }

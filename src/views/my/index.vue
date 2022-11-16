49b8505180489b2b2ac5121eaaa51edd701730ff
chuyu <1575057401@qq.com>
2022-11-16 14:44:26 +0800

    完成个人中心已登录页面的头部

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -6,6 +6,41 @@
         <span class="text">登录/注册</span>
       </div>
     </div>
+
+    <div class="header user-info">
+      <div class="base-info">
+        <div class="left">
+          <van-image
+            round
+            class="avatar"
+            fit="cover"
+            src="https://img01.yzcdn.cn/vant/cat.jpeg"
+          />
+          <span class="name">黑马头条号</span>
+        </div>
+        <div class="right">
+          <van-button size="mini">编辑资料</van-button>
+        </div>
+      </div>
+      <div class="data-status">
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">头条</span>
+        </div>
+       <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">关注</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">粉丝</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">获赞</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -30,14 +65,56 @@ export default {
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    .mobile-img{
-      width:132px;
-      height:132px;
+    .mobile-img {
+      width: 132px;
+      height: 132px;
       margin-bottom: 15px;
     }
-    .text{
-      font-size:28px;
-      color:#fff
+    .text {
+      font-size: 28px;
+      color: #fff;
+    }
+  }
+}
+.user-info {
+  .base-info {
+    box-sizing: border-box;
+    height: 231px;
+    padding: 76px 32px 23px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .left {
+      display: flex;
+      align-items: center;
+      .avatar{
+        width:132px;
+        height: 132px;
+        margin-right: 23px;
+        border: 3px solid #fff;
+      }
+      .name{
+        font-size: 30px;
+        color: #fff;
+      }
+    }
+  }
+  .data-status {
+    display: flex;
+    .data-item {
+      height: 130px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      color:#fff;
+      flex:1;
+      .count{
+        font-size:36px;
+      }
+      .text{
+        font-size: 23px;
+      }
     }
   }
 }
